8296f886f248946e2c7b7bcb84c78dabe0afc17d
Frank Thamel <frankthamel@franks-imac.local>
2016-07-25 21:43:26 +0530

    code added

--- Structs.playground/Contents.swift
+++ Structs.playground/Contents.swift
@@ -2,4 +2,44 @@
 
 import UIKit
 
-var str = "Hello, playground"
+struct  Contact {
+    var firstName : String
+    var lastName :  String
+}
+
+var person  = Contact(firstName: "Frank", lastName: "Thamel")
+person.firstName = "Jude"
+person.firstName
+
+//initializers
+
+struct  Address {
+    var street :  String
+    var city : String
+    var country = "Sri Lanka"
+    
+    init(street : String , city : String){
+        self.street = street
+        self.city = city
+    }
+    
+    init(street : String , city : String , country : String){
+        self.street = street
+        self.city = city
+        self.country = country
+    }
+    
+    func getFullAddress() -> String {
+        return self.street + ", " + self.city + ", " + self.country
+    }
+}
+
+var myAddress = Address(street: "12", city: "CW", country: "SL")
+myAddress.getFullAddress()
+
+var myNewAddress =  Address(street: "12", city: "CO")
+myNewAddress.getFullAddress()
+
+
+
+
