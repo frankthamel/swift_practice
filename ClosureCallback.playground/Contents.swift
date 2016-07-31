230205e8915063e780859cf1e0b439aef9df2a5a
Frank Thamel <frankthamel@franks-imac.local>
2016-07-31 07:48:44 +0530

    Completed

--- ClosureCallback.playground/Contents.swift
+++ ClosureCallback.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+let URL =  NSURL(string: "http://blog.netgloo.com/2015/07/09/spring-boot-communications-link-failure-with-mysql-and-hibernate/")
+let request = NSURLRequest(URL: URL!)
+
+//let downloadTask = session.downloadTaskWithRequest(request , completionHandler: {
+//    (url : NSURL! , response : NSURLResponse! , error : NSError!)  -> Void in
+//    
+//})
+
+let newDownloadTask = session.downloadTaskWithRequest(request){
+    (let url , let response , let error) in
+    print(response)
+}
+
+//downloadTask.resume()
+newDownloadTask.resume()
+
+typealias   JSONDictionaryCompletion  =  ([String : AnyObject]?) -> ()
+
+func downloadJsonFromWeb ( completion : JSONDictionaryCompletion) {
+    
+}
